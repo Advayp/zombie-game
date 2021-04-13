7e1617a04ab0a075d2698de375553e1a6762e59d
Advay Patil <advaypatil27@gmail.com>
2021-04-13 11:47:29 -0700

    Added Basic Player Movement

---
+++ Zombie Game/Assets/_Project/Scripts/Player/MouseLook.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    [SerializeField] private float mouseSensitivity;
+    [SerializeField] private Transform player;
+
+    float xRotation = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+    
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90, 90);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+        player.Rotate(Vector3.up * mouseX);
+
+    }
+}
